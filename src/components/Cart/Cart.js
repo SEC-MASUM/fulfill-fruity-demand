5cc48203d9687088a90e160a4fd0ead6bece1342
SEC-MASUM <masum.jess42@gmail.com>
2022-03-26 13:11:20 +0600

    cart section design and button added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,6 +5,13 @@ const Cart = () => {
   return (
     <div className="cart">
       <h3>Selected Fruits</h3>
+      <div className="cart-items"></div>
+
+      <div className="card-button">
+        <button className="choose-for-me">Choose 1 for me</button>
+        <br />
+        <button className="choose-again">Choose again</button>
+      </div>
     </div>
   );
 };
