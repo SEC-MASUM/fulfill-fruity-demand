aef35461db25f63f895eb894a3d322b55a9d8b59
SEC-MASUM <masum.jess42@gmail.com>
2022-03-26 21:30:44 +0600

    1st question's answer done

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -8,7 +8,17 @@ const Question = () => {
       <div className="questions-container">
         <div className="question-container">
           <h1 className="question">How React works?</h1>
-          <p className="answer">sfsdfsdfsd</p>
+          <p className="answer">
+            React হচ্ছে user interface তৈরির জন্য JS এর একটা জনপ্রিয় Library.
+            User interface তৈরির জন্য HTML কোড ব্যবহার করা হয়। কিন্তু React, JS
+            code কে HTML কোড এ convert করে user interface তৈর করে। এই কাজের জন্য
+            'React' নামের package use হয়। HTML code কে browser এর real dom এ
+            render করার আগেই সে নিজে একটা virtual dom create করে 'ReactDom'
+            নামের package use করে। আর এই virtual DOM এর ভিতর যখন কোনো পরিবর্তন
+            হয়, সেইটা কে real DOM এর কপি এর সাথে তুলনা করে ঠিক যতটুকু change পায়
+            ততটুকুই real DOM এ render করে। এই কাজ এর জন্য পুরা page টা reload
+            দেয়া লাগে না। এইটাই react এর beauty.
+          </p>
         </div>
         <div className="question-container">
           <h1 className="question">Props vs State</h1>
@@ -24,3 +34,15 @@ const Question = () => {
 };
 
 export default Question;
+/* 
+  Props: 
+  1. 
+  2.
+  3.
+  4.
+  State: 
+  1.
+  2.
+  3.
+  4.
+*/
